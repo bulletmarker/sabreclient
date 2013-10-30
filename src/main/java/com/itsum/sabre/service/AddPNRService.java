faef9e7085fbabe1f0f7b31aa62c0f8f2baa5dab
changsheng.ma <bulletmarker@qq.com>
2013-10-30 17:00:03 +0800

    2013/10/30

---
+++ src/main/java/com/itsum/sabre/service/AddPNRService.java
@@ -0,0 +1,90 @@
+package com.itsum.sabre.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.itsum.sabre.client.connection.SabreConnection;
+import com.itsum.sabre.client.dto.endtransaction.EndTransactionLLSInput;
+import com.itsum.sabre.client.dto.endtransaction.EndTransactionLLSOutput;
+import com.itsum.sabre.client.dto.endtransaction.EndTransactionRQ;
+import com.itsum.sabre.client.dto.endtransaction.EndTransactionRS;
+import com.itsum.sabre.client.dto.ota.airbook.OTAAirBookRQ;
+import com.itsum.sabre.client.dto.ota.airbook.OTA_AirBookLLSInput;
+import com.itsum.sabre.client.dto.ota.airbook.OTA_AirBookLLSOutput;
+import com.itsum.sabre.client.dto.travelitineraryaddinfo.TravelItineraryAddInfoLLSInput;
+import com.itsum.sabre.client.dto.travelitineraryaddinfo.TravelItineraryAddInfoLLSOutput;
+import com.itsum.sabre.client.dto.travelitineraryaddinfo.TravelItineraryAddInfoRQ;
+import com.itsum.sabre.client.endpoint.EndTransactionLLSEP;
+import com.itsum.sabre.client.endpoint.OTA_AirBookLLSEP;
+import com.itsum.sabre.client.endpoint.TravelItineraryAddInfoLLSEP;
+import com.itsum.sabre.client.exception.SabreClientException;
+
+/**
+ * 添加行程信息服务
+* @ClassName: TravelItineraryAddInfoService
+* @Description: TODO
+* @author Jason.ma
+* @date 2013年10月30日 下午4:30:22
+*
+ */
+public class AddPNRService {
+	
+	private static Logger logger = LoggerFactory.getLogger(AddPNRService.class);
+
+	public static String addInfo(SabreConnection conn,TravelItineraryAddInfoRQ travelItineraryAddInfoRQ,OTAAirBookRQ oTAAirBookRQ)
+			throws SabreClientException {
+		TravelItineraryAddInfoLLSInput travelItineraryAddInfoLLSInput = new TravelItineraryAddInfoLLSInput();
+		travelItineraryAddInfoLLSInput.setRq(travelItineraryAddInfoRQ);
+		TravelItineraryAddInfoLLSOutput travelItineraryAddInfoLLSOutput = TravelItineraryAddInfoLLSEP.getInstance().call(conn, travelItineraryAddInfoLLSInput);
+		if (travelItineraryAddInfoLLSOutput != null && travelItineraryAddInfoLLSOutput.isSuccess()) {
+			logger.debug("TravelItineraryAddInfoLLS调用成功");
+		} else {
+			throw new SabreClientException(
+					travelItineraryAddInfoLLSOutput.getErrorCode(),
+					travelItineraryAddInfoLLSOutput.getErrorMessage(),
+					travelItineraryAddInfoLLSOutput.getSeverity(),
+					travelItineraryAddInfoLLSOutput.getErrorInfo());
+		}
+		
+		OTA_AirBookLLSInput oTA_AirBookLLSInput = new OTA_AirBookLLSInput();
+		oTA_AirBookLLSInput.setRq(oTAAirBookRQ);
+		OTA_AirBookLLSOutput oTA_AirBookLLSOutput = OTA_AirBookLLSEP.getInstance().call(conn, oTA_AirBookLLSInput);
+		if(oTA_AirBookLLSOutput != null && oTA_AirBookLLSOutput.isSuccess()){
+			logger.debug("OTA_AirBookLLS调用成功");
+		}else{
+			throw new SabreClientException(
+					oTA_AirBookLLSOutput.getErrorCode(),
+					oTA_AirBookLLSOutput.getErrorMessage(),
+					oTA_AirBookLLSOutput.getSeverity(),
+					oTA_AirBookLLSOutput.getErrorInfo());
+		}
+		
+		//提交事务
+		EndTransactionLLSInput endTransactionLLSInput = new EndTransactionLLSInput();
+		EndTransactionRQ endTransactionRQ = new EndTransactionRQ();
+		EndTransactionRQ.EndTransaction endTransation = new EndTransactionRQ.EndTransaction();
+		endTransation.setInd(true);//是否提交事务
+		EndTransactionRQ.EndTransaction.Email receiveEmail = new EndTransactionRQ.EndTransaction.Email();
+		receiveEmail.setInd(true);//邮件提醒
+		endTransation.setEmail(receiveEmail);
+		endTransactionRQ.setEndTransaction(endTransation);
+		EndTransactionRQ.Source source = new EndTransactionRQ.Source();
+		source.setReceivedFrom("LIN HOWARD");
+		endTransactionRQ.setSource(source);
+		endTransactionLLSInput.setRq(endTransactionRQ);
+		EndTransactionLLSOutput endTransactionLLSOutput = EndTransactionLLSEP.getInstance().call(conn, endTransactionLLSInput);
+		if(endTransactionLLSOutput != null && endTransactionLLSOutput.isSuccess()){
+			EndTransactionRS endTransactionRS = endTransactionLLSOutput.getRs();
+			String pnr = endTransactionRS.getItineraryRef().getID();
+			logger.debug("EndTransactionLLS调用成功,pnr:"+ pnr);
+			return pnr;
+		}else{
+			throw new SabreClientException(
+					endTransactionLLSOutput.getErrorCode(),
+					endTransactionLLSOutput.getErrorMessage(),
+					endTransactionLLSOutput.getSeverity(),
+					endTransactionLLSOutput.getErrorInfo());
+		}
+	}
+
+}
--- src/test/java/com/itsum/sabre/client/test/process/TPFC_WorkFlowTest.java
+++ src/test/java/com/itsum/sabre/client/test/process/TPFC_WorkFlowTest.java
@@ -16,7 +16,7 @@ import com.itsum.sabre.client.exception.SabreClientException;
 import com.itsum.sabre.client.test.InitConfig;
 import com.itsum.sabre.service.OTA_AirBookService;
 import com.itsum.sabre.service.OTA_AirPriceService;
-import com.itsum.sabre.service.TravelItineraryAddService;
+import com.itsum.sabre.service.AddPNRService;
 
 public class TPFC_WorkFlowTest {
 	
@@ -30,103 +30,103 @@ public class TPFC_WorkFlowTest {
 		InitConfig.init();
 		SabreConnection conn = null;
 		try {
-			conn = SabreConnectionFactory.openConnection();
-			//添加行程基本信息
-			TravelItineraryAddInfoLLSInput travelItineraryAddInfoLLSInput = new TravelItineraryAddInfoLLSInput();
-			TravelItineraryAddInfoRQ travelItineraryAddInfoRQ = new TravelItineraryAddInfoRQ();
-			TravelItineraryAddInfoRQ.AgencyInfo agencyInfo = new TravelItineraryAddInfoRQ.AgencyInfo();
-			TravelItineraryAddInfoRQ.AgencyInfo.Address address = new TravelItineraryAddInfoRQ.AgencyInfo.Address();
-			address.setAddressLine("SIGNET TRAVEL");
-			address.setCityName("LOS ANGELES");
-			address.setCountryCode("US");
-			address.setPostalCode("91733");
-			TravelItineraryAddInfoRQ.AgencyInfo.Address.StateCountyProv stateCountyProv = new TravelItineraryAddInfoRQ.AgencyInfo.Address.StateCountyProv();
-			stateCountyProv.setStateCode("CA");
-			address.setStateCountyProv(stateCountyProv);
-			address.setStreetNmbr("1170 Durfee Ave. Suite ES. El Monte");
-			TravelItineraryAddInfoRQ.AgencyInfo.Address.VendorPrefs vendorPrefs = new TravelItineraryAddInfoRQ.AgencyInfo.Address.VendorPrefs();
-			TravelItineraryAddInfoRQ.AgencyInfo.Address.VendorPrefs.Airline airline = new TravelItineraryAddInfoRQ.AgencyInfo.Address.VendorPrefs.Airline();
-			airline.setHosted(true);
-			vendorPrefs.setAirline(airline);
-			address.setVendorPrefs(vendorPrefs);
-			agencyInfo.setAddress(address);
-			
-			TravelItineraryAddInfoRQ.AgencyInfo.Ticketing ticketing = new TravelItineraryAddInfoRQ.AgencyInfo.Ticketing();
-			ticketing.setPseudoCityCode("IH9A");
-			ticketing.setTicketType("7TAW");
-			ticketing.setPseudoCityCode(conn.getCpaId());
-			ticketing.setQueueNumber("55");
-			ticketing.setShortText("TEST");
-			
-			agencyInfo.setTicketing(ticketing);
-			travelItineraryAddInfoRQ.setAgencyInfo(agencyInfo);
-			
-			TravelItineraryAddInfoRQ.CustomerInfo customerInfo = new TravelItineraryAddInfoRQ.CustomerInfo();
-			TravelItineraryAddInfoRQ.CustomerInfo.Email email = new TravelItineraryAddInfoRQ.CustomerInfo.Email();
-			email.setAddress("HOWARD718@HOTMAIL.COM");
-			email.setNameNumber("1.1");
-			List<TravelItineraryAddInfoRQ.CustomerInfo.Email> emails = customerInfo.getEmail();
-			emails.add(email);
-			
-			List<TravelItineraryAddInfoRQ.CustomerInfo.PersonName> personNames = customerInfo.getPersonName();
-			TravelItineraryAddInfoRQ.CustomerInfo.PersonName personName = new TravelItineraryAddInfoRQ.CustomerInfo.PersonName();
-			personName.setNameReference("TEST");
-			personName.setGivenName("HOWARD");
-			personName.setSurname("LINL");
-			personNames.add(personName);
-			
-			TravelItineraryAddInfoRQ.CustomerInfo.ContactNumbers contactNumbers = new TravelItineraryAddInfoRQ.CustomerInfo.ContactNumbers();
-			List<TravelItineraryAddInfoRQ.CustomerInfo.ContactNumbers.ContactNumber> contactNumbersL= contactNumbers.getContactNumber();
-			TravelItineraryAddInfoRQ.CustomerInfo.ContactNumbers.ContactNumber contactNumber = new TravelItineraryAddInfoRQ.CustomerInfo.ContactNumbers.ContactNumber();
-			contactNumber.setPhone("6052187");
-			contactNumber.setPhoneUseType("B");
-			contactNumbersL.add(contactNumber);
-			customerInfo.setContactNumbers(contactNumbers);
-			travelItineraryAddInfoRQ.setCustomerInfo(customerInfo);
-			travelItineraryAddInfoLLSInput.setRq(travelItineraryAddInfoRQ);
-			TravelItineraryAddService.createPNR(conn, travelItineraryAddInfoLLSInput);
-			logger.info("TravelItineraryAddService 调用成功");
-			
-			//添加行程预定信息
-			OTAAirBookRQ oTAAirBookRQ = new OTAAirBookRQ();
-			OTAAirBookRQ.OriginDestinationInformation originDestinationInformation = new OTAAirBookRQ.OriginDestinationInformation();
-			OTAAirBookRQ.OriginDestinationInformation.FlightSegment flightSegment = new OTAAirBookRQ.OriginDestinationInformation.FlightSegment();
-			flightSegment.setDepartureDateTime("2013-10-18T12:10");
-			flightSegment.setArrivalDateTime("2013-10-18T13:35");
-			flightSegment.setFlightNumber("566");
-			OTAAirBookRQ.OriginDestinationInformation.FlightSegment.OriginLocation f1OriginLocation = new OTAAirBookRQ.OriginDestinationInformation.FlightSegment.OriginLocation();
-			f1OriginLocation.setLocationCode("LAX");
-			flightSegment.setOriginLocation(f1OriginLocation);
-			OTAAirBookRQ.OriginDestinationInformation.FlightSegment.DestinationLocation d1DestinationLocation = new OTAAirBookRQ.OriginDestinationInformation.FlightSegment.DestinationLocation();
-			d1DestinationLocation.setLocationCode("SFO");
-			flightSegment.setDestinationLocation(d1DestinationLocation);
-			flightSegment.setNumberInParty("1");
-			flightSegment.setStatus("NN");
-			flightSegment.setResBookDesigCode("Y");
-			
-			OTAAirBookRQ.OriginDestinationInformation.FlightSegment.DestinationLocation destinationLocation = new OTAAirBookRQ.OriginDestinationInformation.FlightSegment.DestinationLocation();
-			destinationLocation.setLocationCode("SFO");
-			flightSegment.setDestinationLocation(destinationLocation);
-			OTAAirBookRQ.OriginDestinationInformation.FlightSegment.MarketingAirline marketingAirline = new OTAAirBookRQ.OriginDestinationInformation.FlightSegment.MarketingAirline();
-			marketingAirline.setFlightNumber("356");
-			marketingAirline.setCode("UA");
-			flightSegment.setMarketingAirline(marketingAirline);
-			OTAAirBookRQ.OriginDestinationInformation.FlightSegment.OriginLocation originLocation = new OTAAirBookRQ.OriginDestinationInformation.FlightSegment.OriginLocation();
-			originLocation.setLocationCode("LAX");
-			flightSegment.setOriginLocation(originLocation);
-			List<OTAAirBookRQ.OriginDestinationInformation.FlightSegment> flightSegments =originDestinationInformation.getFlightSegment();
-			flightSegments.add(flightSegment);
-			oTAAirBookRQ.setOriginDestinationInformation(originDestinationInformation);
-			OTA_AirBookLLSInput oTA_AirBookLLSInput = new OTA_AirBookLLSInput();
-			oTA_AirBookLLSInput.setRq(oTAAirBookRQ);
-			OTA_AirBookService.bookAir(conn, oTA_AirBookLLSInput);
-			logger.info("TravelItineraryAddService 调用成功");
-			
-			OTA_AirPriceService.priceAir(conn, "ADT", "1");
-			logger.info("OTA_AirPriceService 调用成功");
-			
-			conn.commit("LIN HOWARD");
-			logger.info("EndTransactionService 调用成功");
+//			conn = SabreConnectionFactory.openConnection();
+//			//添加行程基本信息
+//			TravelItineraryAddInfoLLSInput travelItineraryAddInfoLLSInput = new TravelItineraryAddInfoLLSInput();
+//			TravelItineraryAddInfoRQ travelItineraryAddInfoRQ = new TravelItineraryAddInfoRQ();
+//			TravelItineraryAddInfoRQ.AgencyInfo agencyInfo = new TravelItineraryAddInfoRQ.AgencyInfo();
+//			TravelItineraryAddInfoRQ.AgencyInfo.Address address = new TravelItineraryAddInfoRQ.AgencyInfo.Address();
+//			address.setAddressLine("SIGNET TRAVEL");
+//			address.setCityName("LOS ANGELES");
+//			address.setCountryCode("US");
+//			address.setPostalCode("91733");
+//			TravelItineraryAddInfoRQ.AgencyInfo.Address.StateCountyProv stateCountyProv = new TravelItineraryAddInfoRQ.AgencyInfo.Address.StateCountyProv();
+//			stateCountyProv.setStateCode("CA");
+//			address.setStateCountyProv(stateCountyProv);
+//			address.setStreetNmbr("1170 Durfee Ave. Suite ES. El Monte");
+//			TravelItineraryAddInfoRQ.AgencyInfo.Address.VendorPrefs vendorPrefs = new TravelItineraryAddInfoRQ.AgencyInfo.Address.VendorPrefs();
+//			TravelItineraryAddInfoRQ.AgencyInfo.Address.VendorPrefs.Airline airline = new TravelItineraryAddInfoRQ.AgencyInfo.Address.VendorPrefs.Airline();
+//			airline.setHosted(true);
+//			vendorPrefs.setAirline(airline);
+//			address.setVendorPrefs(vendorPrefs);
+//			agencyInfo.setAddress(address);
+//			
+//			TravelItineraryAddInfoRQ.AgencyInfo.Ticketing ticketing = new TravelItineraryAddInfoRQ.AgencyInfo.Ticketing();
+//			ticketing.setPseudoCityCode("IH9A");
+//			ticketing.setTicketType("7TAW");
+//			ticketing.setPseudoCityCode(conn.getCpaId());
+//			ticketing.setQueueNumber("55");
+//			ticketing.setShortText("TEST");
+//			
+//			agencyInfo.setTicketing(ticketing);
+//			travelItineraryAddInfoRQ.setAgencyInfo(agencyInfo);
+//			
+//			TravelItineraryAddInfoRQ.CustomerInfo customerInfo = new TravelItineraryAddInfoRQ.CustomerInfo();
+//			TravelItineraryAddInfoRQ.CustomerInfo.Email email = new TravelItineraryAddInfoRQ.CustomerInfo.Email();
+//			email.setAddress("HOWARD718@HOTMAIL.COM");
+//			email.setNameNumber("1.1");
+//			List<TravelItineraryAddInfoRQ.CustomerInfo.Email> emails = customerInfo.getEmail();
+//			emails.add(email);
+//			
+//			List<TravelItineraryAddInfoRQ.CustomerInfo.PersonName> personNames = customerInfo.getPersonName();
+//			TravelItineraryAddInfoRQ.CustomerInfo.PersonName personName = new TravelItineraryAddInfoRQ.CustomerInfo.PersonName();
+//			personName.setNameReference("TEST");
+//			personName.setGivenName("HOWARD");
+//			personName.setSurname("LINL");
+//			personNames.add(personName);
+//			
+//			TravelItineraryAddInfoRQ.CustomerInfo.ContactNumbers contactNumbers = new TravelItineraryAddInfoRQ.CustomerInfo.ContactNumbers();
+//			List<TravelItineraryAddInfoRQ.CustomerInfo.ContactNumbers.ContactNumber> contactNumbersL= contactNumbers.getContactNumber();
+//			TravelItineraryAddInfoRQ.CustomerInfo.ContactNumbers.ContactNumber contactNumber = new TravelItineraryAddInfoRQ.CustomerInfo.ContactNumbers.ContactNumber();
+//			contactNumber.setPhone("6052187");
+//			contactNumber.setPhoneUseType("B");
+//			contactNumbersL.add(contactNumber);
+//			customerInfo.setContactNumbers(contactNumbers);
+//			travelItineraryAddInfoRQ.setCustomerInfo(customerInfo);
+//			travelItineraryAddInfoLLSInput.setRq(travelItineraryAddInfoRQ);
+//			AddPNRService.createPNR(conn, travelItineraryAddInfoLLSInput);
+//			logger.info("TravelItineraryAddService 调用成功");
+//			
+//			//添加行程预定信息
+//			OTAAirBookRQ oTAAirBookRQ = new OTAAirBookRQ();
+//			OTAAirBookRQ.OriginDestinationInformation originDestinationInformation = new OTAAirBookRQ.OriginDestinationInformation();
+//			OTAAirBookRQ.OriginDestinationInformation.FlightSegment flightSegment = new OTAAirBookRQ.OriginDestinationInformation.FlightSegment();
+//			flightSegment.setDepartureDateTime("2013-10-18T12:10");
+//			flightSegment.setArrivalDateTime("2013-10-18T13:35");
+//			flightSegment.setFlightNumber("566");
+//			OTAAirBookRQ.OriginDestinationInformation.FlightSegment.OriginLocation f1OriginLocation = new OTAAirBookRQ.OriginDestinationInformation.FlightSegment.OriginLocation();
+//			f1OriginLocation.setLocationCode("LAX");
+//			flightSegment.setOriginLocation(f1OriginLocation);
+//			OTAAirBookRQ.OriginDestinationInformation.FlightSegment.DestinationLocation d1DestinationLocation = new OTAAirBookRQ.OriginDestinationInformation.FlightSegment.DestinationLocation();
+//			d1DestinationLocation.setLocationCode("SFO");
+//			flightSegment.setDestinationLocation(d1DestinationLocation);
+//			flightSegment.setNumberInParty("1");
+//			flightSegment.setStatus("NN");
+//			flightSegment.setResBookDesigCode("Y");
+//			
+//			OTAAirBookRQ.OriginDestinationInformation.FlightSegment.DestinationLocation destinationLocation = new OTAAirBookRQ.OriginDestinationInformation.FlightSegment.DestinationLocation();
+//			destinationLocation.setLocationCode("SFO");
+//			flightSegment.setDestinationLocation(destinationLocation);
+//			OTAAirBookRQ.OriginDestinationInformation.FlightSegment.MarketingAirline marketingAirline = new OTAAirBookRQ.OriginDestinationInformation.FlightSegment.MarketingAirline();
+//			marketingAirline.setFlightNumber("356");
+//			marketingAirline.setCode("UA");
+//			flightSegment.setMarketingAirline(marketingAirline);
+//			OTAAirBookRQ.OriginDestinationInformation.FlightSegment.OriginLocation originLocation = new OTAAirBookRQ.OriginDestinationInformation.FlightSegment.OriginLocation();
+//			originLocation.setLocationCode("LAX");
+//			flightSegment.setOriginLocation(originLocation);
+//			List<OTAAirBookRQ.OriginDestinationInformation.FlightSegment> flightSegments =originDestinationInformation.getFlightSegment();
+//			flightSegments.add(flightSegment);
+//			oTAAirBookRQ.setOriginDestinationInformation(originDestinationInformation);
+//			OTA_AirBookLLSInput oTA_AirBookLLSInput = new OTA_AirBookLLSInput();
+//			oTA_AirBookLLSInput.setRq(oTAAirBookRQ);
+//			OTA_AirBookService.bookAir(conn, oTA_AirBookLLSInput);
+//			logger.info("TravelItineraryAddService 调用成功");
+//			
+//			OTA_AirPriceService.priceAir(conn, "ADT", "1");
+//			logger.info("OTA_AirPriceService 调用成功");
+//			
+//			conn.commit("LIN HOWARD");
+//			logger.info("EndTransactionService 调用成功");
 			
 		} catch(SabreClientException e){
 			logger.warn("调用服务失败:");
